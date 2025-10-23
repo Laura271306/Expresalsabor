139fe3f6988bc9213664aaaad29be30c9d8424d7
[dyad] <lauranunes1306@gmail.com>
2025-10-23 16:23:46 -0300

    [dyad] Usando todas as cores da paleta no site - wrote 15 file(s)

--- src/components/ResultsSection.tsx
+++ src/components/ResultsSection.tsx
@@ -63,7 +63,7 @@ const ResultsSection: React.FC = () => {
       </div>
       
       <div className="text-center">
-        <p className="text-orange-600 font-medium mb-6">
+        <p className="font-medium mb-6" style={{ color: '#e58634' }}>
           600+ recetas como estas te esperan
         </p>
         <CtaButton className="text-lg px-8 py-4">
